2b7434b6616d83f67f7858921c01dd6eee4cfe90
crass_sandwich <christopherjanidlo@gmail.com>
2021-04-12 23:09:30 -0400

    Basic ball/paddle interaction

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 Velocity { get; private set; }
+
+    public float Gravity;
+    [Tooltip("Abstract constant that contains every variable in the drag equation that isn't velocity (so it includes coefficient of drag, reference area, and fluid density, which are all held constant throughout the game)")]
+    public float DragCoefficient;
+
+    void FixedUpdate ()
+    {
+        Velocity += Vector2.down * Gravity * Time.deltaTime;
+
+        var dragAcceleration = DragCoefficient * (Velocity.sqrMagnitude / 2) * -Velocity.normalized; // drag equation
+        Velocity += dragAcceleration * Time.deltaTime;
+
+        transform.position += (Vector3) Velocity * Time.deltaTime;
+    }
+
+    public void SetVelocity (Vector2 value)
+    {
+        // TODO: calculate spin
+        Velocity = value;
+    }
+}
---
+++ Assets/Scripts/PaddleSegment.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PaddleSegment : MonoBehaviour
+{
+    public float BounceSpeed;
+    [Range(-90, 90)]
+    public float BounceAngle;
+
+    private void OnCollisionEnter2D (Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Ball>() is Ball ball)
+        {
+            ball.SetVelocity(BounceSpeed * angleToVector(BounceAngle));
+        }
+    }
+
+    Vector2 angleToVector (float angle)
+    {
+        var rad = -angle * Mathf.Deg2Rad;
+        return new Vector2(-Mathf.Sin(rad), Mathf.Cos(rad));
+    }
+}
