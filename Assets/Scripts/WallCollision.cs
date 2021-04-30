8bc0a765ea40f684d37c7dba2637aa6bf11cc6f1
crass_sandwich <christopherjanidlo@gmail.com>
2021-04-29 21:54:55 -0400

    Tighten wall collisions
    They should no longer suck anymore woo

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -1,17 +1,18 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WallCollision : MonoBehaviour
 {
-    void OnCollisionEnter2D (Collision2D collision)
+    // uses Stay instead of Enter because each level's wall is a big composite collider, and sometimes you hit one block of it and bounce into another block the same frame. would rather bounce a frame late than only bounce the first frame we're touching the wall 
+    void OnCollisionStay2D (Collision2D collision)
     {
         var ball = collision.gameObject.GetComponent<Ball>();
         if (ball == null) return;
 
-        // negative since normal is from the perspective of the ball
-        var normal = -collision.GetContact(0).normal;
+        var normal = -getStraightestNormal(collision); // flip it to get it from the perspective of the wall instead of the ball
         var resultingSpeed = Vector2.Reflect(ball.Velocity, normal);
 
         var cardinalNormal = Mathf.Abs(normal.x) > Mathf.Abs(normal.y)
@@ -19,5 +20,32 @@ public class WallCollision : MonoBehaviour
             : new Vector2Int(0, Math.Sign(normal.y));
 
         ball.Bounce(resultingSpeed, cardinalNormal);
+    }
+
+    Vector2 getStraightestNormal (Collision2D collision)
+    {
+        return collision.contacts
+            .Select(c => c.normal)
+            .OrderBy(n => Mathf.Abs(Mathf.Abs(n.y) - 1)) // order by how close to up/down the y coordinate is first because I'd rather have ceiling/floor collisions act predictably than side wall collisions, since I have to choose
+            .ThenBy(n => Mathf.Abs(Mathf.Abs(n.x) - 1))
+            .First();
+    }
+
+    // keeping for reference sake. should never be included in committed code
+    // when multiple rays are drawn, whiter rays are earlier in the contact list
+    void debugCollision (Collision2D collision)
+    {
+        Debug.Log("new collision");
+
+        for (int i = 0; i < collision.contacts.Length; i++)
+        {
+            ContactPoint2D item = collision.contacts[i];
+            Debug.Log("new point");
+            Debug.Log(item.point);
+            Debug.Log(item.normal);
+            Debug.DrawRay(item.point, item.normal * 100, new Color(1f / (i + 1), 1, 1), 1f);
+        }
+
+        Debug.Break();
     }
 }
