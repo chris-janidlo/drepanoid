c34e2b36bdd026adf7f6778d7fd4d57cf5cb9a77
crass_sandwich <christopherjanidlo@gmail.com>
2021-08-21 16:43:59 -0400

    Scaffold tests and add some fake ones to run with CI

---
+++ Assets/Scripts/Test/Runtime/TestTests.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Drepanoid.Test
+{
+    public class TestTests
+    {
+        [Test]
+        public void TestTestsSimplePasses ()
+        {
+            Assert.That(true);
+        }
+
+        [UnityTest]
+        public IEnumerator TestTestsWithEnumeratorPasses ()
+        {
+            yield return null;
+            Assert.That(true);
+        }
+    }
+}
