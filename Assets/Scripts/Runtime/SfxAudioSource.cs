76d608dfefa341677f05a3af6646be64d32293b0
crass_sandwich <christopherjanidlo@gmail.com>
2021-08-22 23:24:30 -0400

    Write out pool and pool member

---
+++ Assets/Scripts/Runtime/SfxAudioSource.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drepanoid
+{
+    public class SfxAudioSource : MonoBehaviour
+    {
+        public bool Playing => audioSource.isPlaying;
+
+        [SerializeField] private AudioSource audioSource;
+
+        void Start ()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void Play (AudioClip clip)
+        {
+            audioSource.PlayOneShot(clip);
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/SoundEffectPlayer.cs
@@ -0,0 +1,28 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Drepanoid
+{
+    [CreateAssetMenu(menuName = "Sound Effect Player", fileName = "newSoundEffectPlayer.asset")]
+    public class SoundEffectPlayer : ScriptableObject
+    {
+        public SfxAudioSource SfxAudioSourcePrefab;
+
+        List<SfxAudioSource> spawnedAudioSources = new List<SfxAudioSource>();
+
+        public void Play (AudioClip clip)
+        {
+            var source = spawnedAudioSources.FirstOrDefault(s => !s.Playing);
+
+            if (source == null)
+            {
+                source = Instantiate(SfxAudioSourcePrefab);
+                spawnedAudioSources.Add(source);
+            }
+
+            source.Play(clip);
+        }
+    }
+}
