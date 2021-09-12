7d5b5a8707302c85906a60b357ab3ffe8ed88dcb
crass_sandwich <christopherjanidlo@gmail.com>
2021-09-12 18:02:17 -0400

    Implement scanline jitter effect

--- Assets/Post Processing/Shaders/Glitch.shader
+++ Assets/Post Processing/Shaders/Glitch.shader
@@ -5,14 +5,29 @@ Shader "Hidden/Custom/Glitch"
         #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
 
         TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-        float _Amount;
+
+        float _ScanlineJitterFrequency;
+        float _ScanlineJitterDisplacement;
+
+        // Pseudo random number generator with 2D coordinates
+        float UVRandom(float u, float v)
+        {
+            return frac(sin(dot(float2(u, v), float2(12.9898,78.233))) * 43758.5453);
+        }
 
         float4 Frag(VaryingsDefault i) : SV_Target
         {
-            float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
-            float luminance = dot(color.rgb, float3(0.2126729, 0.7151522, 0.0721750));
-            color.rgb = lerp(color.rgb, luminance.xxx, _Amount.xxx);
-            return color;
+            // uses a combination of the analogue glitch's scanline jitter and the digital glitch from KinoGlitch (https://github.com/keijiro/KinoGlitch/tree/e102950361c260aba8967d55f14212b5a70f4cce)
+
+            float u = i.texcoord.x;
+            float v = i.texcoord.y;
+
+            float jitter = UVRandom(v, _Time.x) * 2 - 1;
+            jitter *= step(_ScanlineJitterFrequency, abs(jitter)) * _ScanlineJitterDisplacement;
+
+            // TODO: digital glitch
+
+            return SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, float2(frac(u + jitter), v));
         }
 
     ENDHLSL
--- Assets/Scripts/Runtime/Visual Effects/Postprocessing/Glitch.cs
+++ Assets/Scripts/Runtime/Visual Effects/Postprocessing/Glitch.cs
@@ -5,12 +5,12 @@ using UnityEngine.Rendering.PostProcessing;
 namespace Drepanoid
 {
     [Serializable]
-    [PostProcess(typeof(GlitchRenderer), PostProcessEvent.AfterStack, "Custom/Glitch")]
+    [PostProcess(typeof(GlitchRenderer), PostProcessEvent.AfterStack, "Custom/Glitch", allowInSceneView: false)]
     public class Glitch : PostProcessEffectSettings
     {
         [Range(0, 1)]
-        [Tooltip("Currently just controls grayscale intensity as a placeholder")]
-        public FloatParameter Amount = new FloatParameter { value = 0.5f };
+        [Tooltip("Determines the amount and frequency of horizontal displacement")]
+        public FloatParameter ScanlineJitter = new FloatParameter { value = 0.5f };
     }
 
     public class GlitchRenderer : PostProcessEffectRenderer<Glitch>
@@ -18,7 +18,21 @@ namespace Drepanoid
         public override void Render (PostProcessRenderContext context)
         {
             var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/Glitch"));
-            sheet.properties.SetFloat("_Amount", settings.Amount);
+
+            // uses a combination of the analogue glitch's scanline jitter and the digital glitch from KinoGlitch (https://github.com/keijiro/KinoGlitch/tree/e102950361c260aba8967d55f14212b5a70f4cce)
+
+            float jitter = settings.ScanlineJitter;
+
+            // magic constants are from KinoGlitch, help separate overall jitter "intensity" into frequency and displacement values
+            // might want to expose a control for each value instead
+            float jitterFrequency = Mathf.Clamp01(1.0f - jitter * 1.2f);
+            float jitterDisplacement = 0.002f + Mathf.Pow(jitter, 3) * 0.05f;
+
+            sheet.properties.SetFloat("_ScanlineJitterFrequency", jitterFrequency);
+            sheet.properties.SetFloat("_ScanlineJitterDisplacement", jitterDisplacement);
+
+            // TODO: digital glitch
+
             context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
         }
     }
