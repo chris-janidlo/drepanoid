5934d96897ed6da60be65408e511a97d4185a802
crass_sandwich <christopherjanidlo@gmail.com>
2021-10-26 21:34:45 -0500

    Reduce drag when not catching the ball

--- Assets/Scripts/Runtime/Level Elements/Scanline/Scanline.cs
+++ Assets/Scripts/Runtime/Level Elements/Scanline/Scanline.cs
@@ -9,9 +9,9 @@ namespace Drepanoid
     public class Scanline : MonoBehaviour
     {
         public float VerticalAcceleration, InheritSpeedMultiplier;
-        [Tooltip("If the ball's vertical speed is lower than this while in contact with the scanline, the scanline will apply a drag acceleration to both the ball's x and y velocity axes. Otherwise, the scanline only applies its drag vertically.")]
+        public float DragCoefficient, FallCatcherDragCoefficient;
+        [Tooltip("If the ball's vertical speed is lower than this while in contact with the scanline, the scanline will apply a heightened drag acceleration to both the ball's x and y velocity axes. Otherwise, the scanline only applies a lower drag to the y axis.")]
         public float FallCatcherThreshold;
-        public float DragCoefficient;
 
         public float BallMagnetismRange;
         [Range(0, 1)]
@@ -39,7 +39,7 @@ namespace Drepanoid
             if (ball == null) return;
 
             Vector2 dragAcceleration = ball.Velocity.y < FallCatcherThreshold
-                ? DragCoefficient * (ball.Velocity.sqrMagnitude / 2) * -ball.Velocity.normalized
+                ? FallCatcherDragCoefficient * (ball.Velocity.sqrMagnitude / 2) * -ball.Velocity.normalized
                 : DragCoefficient * (ball.Velocity.y * ball.Velocity.y / 2) * Mathf.Sign(ball.Velocity.y) * Vector2.down;
 
             ball.Velocity += (VerticalAcceleration * Vector2.up + dragAcceleration) * Time.deltaTime;
