20e8dd485a7a4d9f8716d53a24f8e79732826c8c
Chris Janidlo <christopherjanidlo@gmail.com>
2021-12-21 22:07:08 -0700

    Move Glitch effect out of post processing stack

--- Assets/Post Processing/Shaders/Glitch.shader
+++ Assets/Post Processing/Shaders/Glitch.shader
@@ -2,50 +2,51 @@ Shader "Hidden/Custom/Glitch"
 {
     // based off the digital glitch from KinoGlitch (https://github.com/keijiro/KinoGlitch/tree/e102950361c260aba8967d55f14212b5a70f4cce)
 
-    HLSLINCLUDE
+    Properties
+    {
+        _MainTex  ("-", 2D) = "" {}
+        _NoiseTex ("-", 2D) = "" {}
+        _TrashTex ("-", 2D) = "" {}
+    }
 
-        #include "Packages/com.unity.postprocessing/PostProcessing/Shaders/StdLib.hlsl"
+    CGINCLUDE
+        #include "UnityCG.cginc"
 
-        TEXTURE2D_SAMPLER2D(_MainTex, sampler_MainTex);
-        TEXTURE2D_SAMPLER2D(_NoiseTex, sampler_NoiseTex);
-        TEXTURE2D_SAMPLER2D(_TrashTex, sampler_TrashTex);
-
+        sampler2D _MainTex;
+        sampler2D _NoiseTex;
+        sampler2D _TrashTex;
         float _Intensity;
 
-        float4 Frag(VaryingsDefault i) : SV_Target
+        float4 frag(v2f_img i) : SV_Target 
         {
-            float2 xy = i.texcoord;
-
-            float4 glitch = SAMPLE_TEXTURE2D(_NoiseTex, sampler_NoiseTex, xy);
+            float4 glitch = tex2D(_NoiseTex, i.uv);
 
             float thresh = 1.001 - _Intensity * 1.001;
             float w_d = step(thresh, pow(glitch.z, 2.5)); // displacement glitch
             float w_f = step(thresh, pow(glitch.w, 2.5)); // frame glitch
 
             // Displacement.
-            float2 uv = frac(xy + glitch.xy * w_d);
-            float4 source = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, uv);
+            float2 uv = frac(i.uv + glitch.xy * w_d);
+            float4 source = tex2D(_MainTex, uv);
 
             // Mix with trash frame.
-            float3 color = lerp(source, SAMPLE_TEXTURE2D(_TrashTex, sampler_TrashTex, uv), w_f).rgb;
+            float3 color = lerp(source, tex2D(_TrashTex, uv), w_f).rgb;
 
             return float4(color, source.a);
         }
 
-    ENDHLSL
+    ENDCG
 
     SubShader
     {
-        Cull Off ZWrite Off ZTest Always
-
         Pass
         {
-            HLSLPROGRAM
-
-                #pragma vertex VertDefault
-                #pragma fragment Frag
-
-            ENDHLSL
+            Cull Off ZWrite Off ZTest Always
+            CGPROGRAM
+                #pragma vertex vert_img
+                #pragma fragment frag
+                #pragma target 3.0
+            ENDCG
         }
     }
 }
--- Assets/Scripts/Runtime/Visual Effects/DeathGlitch.cs
+++ Assets/Scripts/Runtime/Visual Effects/DeathGlitch.cs
@@ -4,13 +4,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 using UnityAtoms.BaseAtoms;
+using crass;
 
 namespace Drepanoid
 {
     public class DeathGlitch : MonoBehaviour
     {
+        // visuals are based off the digital glitch from KinoGlitch (https://github.com/keijiro/KinoGlitch/tree/e102950361c260aba8967d55f14212b5a70f4cce)
+
+        [Header("Visual Effect Settings")]
         public float DeathResetTime;
         public AnimationCurve GlitchIntensityByTimeSinceDeath;
+
+        [Range(0, 1), Tooltip("How \"un-sticky\" the colors of the block-damage effect's noise texture are - the higher the value, the less likely that neighboring pixels are the same color")]
+        public float Noisiness = 0.11f;
+
+        [Range(0, 1)]
+        public float NoiseUpdateChancePerFrame = 0.7f;
+
+        [Tooltip("How many frames between each time the trash frame is updated")]
+        public int
+            TrashFrame1UpdateInterval = 13,
+            TrashFrame2UpdateInterval = 73;
+
+        [Header("References")]
+        public Shader Shader;
 
         public PostProcessProfile PostProcessProfile;
         public RetriggerFilter RetriggerFilter;
@@ -19,12 +37,57 @@ namespace Drepanoid
         public VoidEvent DeathReset;
         public FloatVariable MusicTrackPosition;
 
-        Glitch glitch;
+        Material material;
+
+        float currentIntensity;
+        Texture2D noiseTexture;
+        RenderTexture trashFrame1, trashFrame2;
 
         void Start ()
         {
-            PostProcessProfile.TryGetSettings(out glitch);
             resetEffect();
+
+            material = new Material(Shader) { hideFlags = HideFlags.DontSave };
+
+            noiseTexture = new Texture2D(64, 32, TextureFormat.ARGB32, false)
+            {
+                hideFlags = HideFlags.DontSave,
+                wrapMode = TextureWrapMode.Clamp,
+                filterMode = FilterMode.Point
+            };
+
+            updateNoise();
+
+            trashFrame1 = new RenderTexture(Screen.width, Screen.height, 0) { hideFlags = HideFlags.DontSave };
+            trashFrame2 = new RenderTexture(Screen.width, Screen.height, 0) { hideFlags = HideFlags.DontSave };
+        }
+
+        void Update ()
+        {
+            if (material != null && RandomExtra.Chance(NoiseUpdateChancePerFrame)) updateNoise();
+        }
+
+        void OnRenderImage (RenderTexture source, RenderTexture destination)
+        {
+            void updateTrash (int interval, RenderTexture texture)
+            {
+                if (Time.frameCount % interval == 0) Graphics.Blit(source, texture);
+            }
+
+            updateTrash(TrashFrame1UpdateInterval, trashFrame1);
+            updateTrash(TrashFrame2UpdateInterval, trashFrame2);
+
+            if (!DeathGlitchEffectIsOn.Value)
+            {
+                Graphics.Blit(source, destination);
+                return;
+            }
+
+            material.SetFloat("_Intensity", currentIntensity);
+            material.SetTexture("_NoiseTex", noiseTexture);
+            material.SetTexture("_TrashTex", RandomExtra.Chance(0.5f) ? trashFrame1 : trashFrame2);
+
+            Graphics.Blit(source, destination, material);
         }
 
         public void OnBallDied ()
@@ -44,7 +107,7 @@ namespace Drepanoid
 
             while (timer < totalTime)
             {
-                glitch.Intensity.value = GlitchIntensityByTimeSinceDeath.Evaluate(timer);
+                currentIntensity = GlitchIntensityByTimeSinceDeath.Evaluate(timer);
                 timer += Time.deltaTime;
 
                 if (timer >= DeathResetTime && !haveReset)
@@ -62,9 +125,26 @@ namespace Drepanoid
 
         void resetEffect ()
         {
-            glitch.Intensity.value = 0;
+            currentIntensity = 0;
             DeathGlitchEffectIsOn.Value = false;
             RetriggerFilter.Active = false;
+        }
+
+        void updateNoise ()
+        {
+            static Color randomColor () => new Color(Random.value, Random.value, Random.value, Random.value);
+            Color currentColor = randomColor();
+
+            for (int y = 0; y < noiseTexture.height; y++)
+            {
+                for (int x = 0; x < noiseTexture.width; x++)
+                {
+                    if (RandomExtra.Chance(Noisiness)) currentColor = randomColor();
+                    noiseTexture.SetPixel(x, y, currentColor);
+                }
+            }
+
+            noiseTexture.Apply();
         }
     }
 }
